39fcae3bdf143ce9366ea4e9cd74b176b0e637c3
teej <tj@teej.io>
2021-12-15 23:06:54 -0500

    + day 9
    feel slightly better about this day. still could be optimized but im just trying to catch up in my free time

---
+++ days/9/two.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+from typing import List
+from functools import reduce
+import operator
+
+def main():
+    height_map = process_input('input.txt')
+    low_points = []
+    
+    for i in range(len(height_map)):
+        for j in range(len(height_map[i])):
+            is_low = is_low_point(height_map, i, j)
+            if is_low:
+                low_points.append((i,j))
+
+    basins_sizes = []
+    for low in low_points:
+        counted = set()
+        basin_size = get_basin_size(counted, height_map, low[0], low[1])
+        basins_sizes.append(basin_size)
+    
+    print(reduce(operator.mul, sorted(basins_sizes)[-3:]))
+    
+
+def get_basin_size(counted, height_map, i, j):
+    val = height_map[i][j]
+    if val == 9 or tuple([i,j]) in counted:
+        return 0
+
+    valid_dirs = get_valid_dirs(height_map, i, j)
+    counted.add(tuple([i,j]))
+    
+    size = 1
+    for d in valid_dirs:
+        if d == 'l' and val < height_map[i][j-1]:
+            size += get_basin_size(counted, height_map, i, j-1)
+        if d == 'r' and val < height_map[i][j+1]:
+            size += get_basin_size(counted, height_map, i, j+1)
+        if d == 'u' and val < height_map[i-1][j]:
+            size += get_basin_size(counted, height_map, i-1, j)
+        if d == 'd' and val < height_map[i+1][j]:
+            size += get_basin_size(counted, height_map, i+1, j)
+    return size
+
+def is_low_point(height_map, i, j):
+    is_low = True
+    val = height_map[i][j]
+    valid_dirs = get_valid_dirs(height_map, i, j)
+
+    for d in valid_dirs:
+        if d == 'l' and val >= height_map[i][j-1]:
+            is_low = False
+            break
+        if d == 'r' and val >= height_map[i][j+1]:
+            is_low = False
+            break
+        if d == 'u' and val >= height_map[i-1][j]:
+            is_low = False
+            break
+        if d == 'd' and val >= height_map[i+1][j]:
+            is_low = False
+            break
+    return is_low
+
+def get_valid_dirs(height_map, i, j):
+    valid_dirs = []
+    if i != 0:
+        valid_dirs += 'u'
+    if j != 0:
+        valid_dirs += 'l'
+    if i != len(height_map) - 1:
+        valid_dirs += 'd'
+    if j != len(height_map[i]) - 1:
+        valid_dirs += 'r'
+    return valid_dirs
+  
+def process_input(fileName : str):
+    f = open(fileName, 'r')
+    output = []
+    # 5456789349886456890123985435578996543213456789656899996467789234989765442345789778999989652349879899
+    for line in f:
+        output.append(list(map(int,list(line.strip()))))
+    
+    return output
+
+
+if '__main__' in __name__:
+    main()
